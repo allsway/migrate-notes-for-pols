508e7e0d80aab7a65b976d95e849494fe3b1818d
allsway <ssinick@calstate.edu>
2017-06-27 17:47:30 -0700

    Completed creation of updated XML

--- migrate_pol_notes.py
+++ migrate_pol_notes.py
@@ -15,20 +15,17 @@ def get_base_url():
     return config.get('Params', 'baseurl')
 
 def get_pol_url(id):
-    return get_base_url() + '/acq/po-lines/' + id + '?apiey=' + get_key()
+    return get_base_url() + '/acq/po-lines/' + id + '?apikey=' + get_key()
 
-# Reads each line of the input file
-def read_notes(notes):
-    f  = open(notes,'rt')
-    try:
-        reader = csv.reader(f,delimiter=';')
-        header = next(reader)
-        for row in reader:
-            parse_row(row)
-    finally:
-        f.close()
+# Returns XML for POL if successful GET request
+def get_request(url):
+    results = requests.get(url)
+    if results.status_code != 200:
+        logging.info('Failed to get results for: ' + url)
+    else:
+        return ET.fromstring(results.content)
 
-
+# Adds PAID field labels to each field
 def populate_data(r):
     return_data = ''
     return_data = return_data +  'Paid Date: '  + r[0].strip('"')
@@ -43,21 +40,59 @@ def populate_data(r):
         return_data = return_data +  ' | Note: ' + r[x].replace("\\", "")
     return return_data
 
+# Parses PAID fields into format expected in POL notes
 def parse_row(row):
     notes_array = []
-    id = row[0].split(',')[0]
+    notes_array.append(row[0].split(',')[0])
     first = True
     for r in row:
-        print (r)
+    #    print (r)
         r = r.split(',')
         # first line contains one extra field, remove it from parsing
         if first:
             r = r[1:]
             first = False
         return_data = populate_data(r)
-        print (return_data)
+    #    print (return_data)
         notes_array.append(return_data)
+    print (notes_array)
+    return notes_array
 
+# Reads each line of the input file
+def read_notes(notes):
+    f  = open(notes,'rt')
+    try:
+        reader = csv.reader(f,delimiter=';')
+        header = next(reader)
+        for row in reader:
+            parsed_notes = parse_row(row)
+            add_pol_note(parsed_notes)
+    finally:
+        f.close()
+
+# posts updated POL to Alma API
+def post_pol(url,xml):
+    headers = {"Content-Type": "application/xml"}
+    r = requests.put(url,data=ET.tostring(xml),headers=headers)
+    if r.status_code != 200:
+        logging.info('Failed to post: ' + url)
+    else:
+        print(r.content)
+
+# Iterates through each note, and creates a note element
+def add_pol_note(notes):
+    id = notes[0]
+    del notes[0]
+    url = get_pol_url(id)
+    print (url)
+    xml = get_request(url)
+    notes_node = xml.find("notes")
+    for n in notes:
+        sub = ET.SubElement(notes_node,'note')
+        note_text = ET.SubElement(sub, 'note_text')
+        note_text.text = n
+    print (ET.tostring(xml))
+    post_pol(url,xml)
 
 
 logging.basicConfig(filename='status.log',level=logging.DEBUG)
